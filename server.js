0adf200bf6e2f98dcf6d52151c41f07199461508
Matt H <14212062+mah110020@users.noreply.github.com>
2018-07-10 21:53:09 -0400

    added remaining parts

--- server.js
+++ server.js
@@ -1,39 +1,78 @@
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
+const formidable = require("formidable");
+const parse = require("./parse");
 
 http.createServer((req, res) => {
-	const {url} = req;
+	const {url, method} = req;
 
-	if( url === "/api/sample" ){
+	if( url === "/api/sample" && method === "GET"){
 
 		fs.readFile("api/sample.json", (error, contentBuff) => {
+
 			if( error ){
-
 				res.statusCode = 200;
-				res.write( error.toString() );
-				res.end();
-
-			} else {
-
-				const returnObj = {
-					data: JSON.parse( contentBuff.toString() ),
-					metadata: {
-						generated: new Date().toLocaleString()
-					}
-				};
-
-				res.statusCode = 200;
-				res.write( JSON.stringify(returnObj) );
-				res.end();
-
+				res.end( error.toString() );
+				return;
 			}
+
+			const returnObj = {
+				data: JSON.parse( contentBuff.toString() ),
+				metadata: {
+					generated: new Date().toLocaleString()
+				}
+			};
+
+			res.statusCode = 200;
+			res.end( JSON.stringify(returnObj) );
+
+		});
+
+	} else if( url === "/api/parse" && method === "GET" ){
+
+		res.writeHead(200, {"content-type": "text/html"});
+		res.end(`
+			<form action="/api/parse" method="post" enctype="multipart/form-data">
+				<input type="file" name="uploadfile">
+				<input type="submit">
+			</form>
+		`);
+
+	} else if( url === "/api/parse" && method === "POST" ){
+
+		new formidable.IncomingForm().parse(req, (formError, fields, files) => {
+
+			if( formError ){
+				res.statusCode = 200;
+				res.end( formError.toString() );
+				return;
+			}
+
+			fs.readFile( files.uploadfile.path, (readError, contentBuff) => {
+
+				if( readError ){
+					res.statusCode = 200;
+					res.end( readError.toString() );
+					return;
+				}
+
+				const parsedContents = parse( contentBuff.toString() );
+
+				fs.writeFile("post.json", parsedContents, writeError => {
+					res.statusCode = 200;
+					res.end( writeError ? writeError.toString() : "success" );
+				});
+
+			});
+
 		});
 
 	} else {
+
 		res.statusCode = 404;
-		res.write("404: Not found.");
-		res.end();
+		res.end("404: Not found.");
+
 	}
 
 }).listen(4000);
